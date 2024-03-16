5339f115ae2234f911bca2fcad93bcc50661448d
Nitin Babu <119517452+Nitin3886@users.noreply.github.com>
2024-03-16 14:36:05 +0530

    Create SumAndProduct.java

---
+++ SumAndProduct.java
@@ -0,0 +1,14 @@
+//Java program to calculate the sum and product of two given number.
+
+class SumAndProduct {
+	public static void main(String args[])
+	{
+		int a,b,sum=0,product=0;
+		a=10;
+		b=20;
+		sum=a+b;
+		product=a*b;
+		System.out.println("Sum="+sum);
+		System.out.println("Product="+product);
+	}
+};
