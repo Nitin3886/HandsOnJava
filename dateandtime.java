3bdfe5a6bfb9ed13d3657de694ed83317d989f8c
Nitin Babu <119517452+Nitin3886@users.noreply.github.com>
2024-03-22 13:21:18 +0530

    Create dateandtime.java

---
+++ dateandtime.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Main {
+  public static void main(String[] args) {
+    LocalDateTime myObj = LocalDateTime.now();
+    System.out.println(myObj);
+  }
+}
