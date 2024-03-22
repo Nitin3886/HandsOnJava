9a543926449e0cb34cd55e1b08c443d869ead3e4
Nitin Babu <119517452+Nitin3886@users.noreply.github.com>
2024-03-22 13:45:39 +0530

    Create Nanosecond.java

---
+++ Nanosecond.java
@@ -0,0 +1,9 @@
+// java.time.ZoneId Class in Java with example 
+import java.time.*; 
+public class NanoSecond { 
+	public static void main(String[] args) 
+	{ 
+    LocalTime time3=LocalTime.of(16,20,15, 500000000);
+		System.out.println("Time3 : "+time3);
+	} 
+}
