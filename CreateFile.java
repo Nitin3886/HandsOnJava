bf8b7ed18e112b0a332c829794c1a4da3aedc1ad
Nitin Babu <119517452+Nitin3886@users.noreply.github.com>
2024-04-12 13:02:26 +0530

    Create CreateFile.java

---
+++ CreateFile.java
@@ -0,0 +1,8 @@
+import java.io.File;
+class FileExample {
+    public static void main(String[] args)
+    {
+        File obj = new File("myfile.txt");
+          System.out.println("File Created!");
+    }
+}
